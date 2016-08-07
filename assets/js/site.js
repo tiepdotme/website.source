754dfa4b3ef2e32de7a9885447f1be5a6afd3b5a
niksmac <nikhil@lightrains.com>
2016-08-07 12:31:50 +0530

    Modified css

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function($) {
   jQuery('.dropdown').dropdown();
-  $('.special.cards .image').dimmer({on: 'hover'});
   $('.ui.embed').embed();
 
   $('#mobmenu').on('click', function(e) {
